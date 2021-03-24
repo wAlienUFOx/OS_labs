255b115d0778ea02b3d7f016db763517a10a8d59
wAlienUFOx <47860127+wAlienUFOx@users.noreply.github.com>
2021-03-24 22:11:36 +0300

    Add files via upload

---
+++ lab6/s_func.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <zconf.h>
+#include "zmq.hpp"
+#include "zmq.h"
+
+bool send_message(zmq::socket_t& socket, const std::string& message_string);
+
+std::string recieve_message(zmq::socket_t& socket);
+
+std::string get_port_name(int port);
+
+int bind_socket(zmq::socket_t& socket);
+
+void create_node(int id, int port);
